2a1673d46f98f3a258269d47242c64f1966aa118
CMCCVR <99026597+CMCCVR@users.noreply.github.com>
2022-07-26 20:25:21 -0300

    ModalTemplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
+import { ModalTemplate } from './Components/ModalTemplate';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        <Button variant="primary">Primary</Button>{' '}
-          Meu primeiro projeto - Carlos Miguel
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ModalTemplate/>
     </div>
   );
 }
